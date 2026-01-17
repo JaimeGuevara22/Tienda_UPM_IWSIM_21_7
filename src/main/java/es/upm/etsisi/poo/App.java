8226856b0c59e405d6bcd02b42fcd27f62d65756
Carlos Llorente Vaquero <ca.llorente@alumnos.upm.es>
2026-01-17 21:13:47 +0100

    Merge hecho

--- src/main/java/es/upm/etsisi/poo/App.java
+++ src/main/java/es/upm/etsisi/poo/App.java
@@ -91,7 +91,6 @@ public class App {
             e.printStackTrace();
         } finally {
             if (sc != null) sc.close();
-            System.out.println("prueba");
         }
     }
     private static void help() {
